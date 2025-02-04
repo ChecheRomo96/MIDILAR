b8528227dd7e033e7a4f1dc2c92ee7a8d10cf546
ChecheRomo96 <ChecheRomo96@users.noreply.github.com>
2025-02-04 07:33:54 +0000

    deploy: f42f34200500c48a85384b19197278416d076eee

--- search/all_2.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['callbackstatus_6',['callbackStatus',['../d7/dc6/classMIDILAR_1_1SystemFoundation_1_1Clock.html#a8edcae1e393786b3f98be33cc3905aec',1,'MIDILAR::SystemFoundation::Clock']]],
-  ['capabilities_7',['Capabilities',['../d0/dff/classMIDILAR_1_1MidiProcessor.html#a044e4037c60b4f10e7993aecbddaed71',1,'MIDILAR::MidiProcessor']]],
-  ['clock_8',['Clock',['../d7/dc6/classMIDILAR_1_1SystemFoundation_1_1Clock.html#aba38ca472d9af40c7887f6cc5ac83fba',1,'MIDILAR::SystemFoundation::Clock::Clock()'],['../d7/dc6/classMIDILAR_1_1SystemFoundation_1_1Clock.html#af5bfb4978b2e6c428d96f82b9b82221b',1,'MIDILAR::SystemFoundation::Clock::Clock(HardwareCallback hc, float freq)'],['../d7/dc6/classMIDILAR_1_1SystemFoundation_1_1Clock.html',1,'MIDILAR::SystemFoundation::Clock']]],
-  ['clock_2ecpp_9',['Clock.cpp',['../d0/d6a/Clock_8cpp.html',1,'']]],
-  ['clock_2eh_10',['Clock.h',['../d2/d52/Clock_8h.html',1,'']]],
-  ['clocktick_11',['ClockTick',['../d0/dff/classMIDILAR_1_1MidiProcessor.html#a1584d7dae3ba101b9a280a054e17cb86',1,'MIDILAR::MidiProcessor']]]
-];
--- search/all_c.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['timebase_48',['Timebase',['../d7/dc6/classMIDILAR_1_1SystemFoundation_1_1Clock.html#a19cc43a05f6349d3ec1775925e730045',1,'MIDILAR::SystemFoundation::Clock']]],
-  ['timepoint_49',['TimePoint',['../d7/dc6/classMIDILAR_1_1SystemFoundation_1_1Clock.html#ad3f49de0e2009c7b009b594261b2a155',1,'MIDILAR::SystemFoundation::Clock']]]
-];
