00158ea1f4858908afb8ef2249c39d75e8c22d93
José Manuel Romo <josemanuel.romo.96@gmail.com>
2025-02-07 04:23:13 -0600

    .

--- src/MidiProcessors/ChannelReassign/ChannelReassign.cpp
+++ src/MidiProcessors/ChannelReassign/ChannelReassign.cpp
@@ -9,7 +9,7 @@ namespace MIDILAR::MidiProcessors{
 	                        static_cast<uint32_t>(Capabilities::MidiOut));
 
 	        _MessageParser.BindChannelVoiceCallback(
-			    static_cast<CallbackReferenceType>(nullptr),  	// We don't need a `CallbackReferenceType` here
+			    static_cast<MidiFoundation::MessageParser::CallbackReferenceType>(nullptr),  	// We don't need a `CallbackReferenceType` here
 			    static_cast<void*>(this),  						// Pass `this` as the instance
 			    &ChannelReassign::StaticChannelVoiceCallback  	// Pass static function pointer
 			);
