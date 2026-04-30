8f98f766a00cf58b9badc189101344326c433de2
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2026-04-29 18:57:20 -0600

    .

--- STYLE_GUIDE.md
+++ STYLE_GUIDE.md
@@ -3,18 +3,22 @@
 ## Naming Conventions
 
 ### Classes
+
 - Use PascalCase
 - Example: `LUT1D`, `VelocityShaper`
 
 ### Methods
+
 - Use PascalCase
 - Example: `SetBinValue()`, `GetValue()`
 
 ### Variables
+
 - Use snake_case with leading underscore for private members
 - Example: `_buffer`, `_capacity`
 
 ### Macros
+
 - Use UPPER_CASE
 - Example: `MIDILAR_ENABLE_LUT`
 
--- examples/dspFoundation/Generators/Shaping/Atan_Buffer/CMake/Atan_Buffer.cpp
+++ examples/dspFoundation/Generators/Shaping/Atan_Buffer/CMake/Atan_Buffer.cpp
@@ -1,4 +1,4 @@
-#include <MIDILAR/dspFoundation/Generators/Shaping/Atan.h>
+#include <dspFoundation/Generators/Shaping/Atan.h>
 #include <stdio.h>
 #include <stddef.h>
 
@@ -9,7 +9,7 @@ int main() {
     const float Drive = 4.0f;
     const float Bias = 0.25f;
 
-    MIDILAR::dspFoundation::Generators::Shaping::Atan<float>::Generate(
+    MIDILAR::dspFoundation::Generators::Shaping::Atan<float>(
         buffer,
         BufferSize,
         Drive, Bias
